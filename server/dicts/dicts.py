6632df83c8b4d4a3e815cd967dcac37ba63956b9
theczipson <krystekp131@interia.pl>
2025-03-16 23:10:48 +0100

    remove unnecessary comments

--- server/dicts/dicts.py
+++ server/dicts/dicts.py
@@ -1,7 +1,5 @@
-from flask import request, jsonify, session, make_response, Blueprint
-from app import db
+from flask import jsonify, Blueprint
 from db_models.database_tables import DPostalCode
-# from server.dicts import dicts
 
 dicts = Blueprint("dicts", __name__)
 
@@ -11,12 +9,3 @@ def city(postal_code):
     places = DPostalCode.query.filter(DPostalCode.postal_code.like(f'%{postal_code}%')).all()
     response = [{"place": place.place, "postal_code": place.postal_code} for place in places]
     return jsonify(response)
-
-@dicts.route('/health', methods=['GET'])
-def health_check():
-    try:
-        # Try to query the database to check connection
-        db.session.execute('SELECT 1')  # This is a simple query to check if the database is reachable
-        return jsonify({"status": "success", "message": "Database connection is working."}), 200
-    except Exception as e:
-        return jsonify({"status": "error", "message": f"Database connection failed: {str(e)}"}), 500
