fad51571ab893fa50476971c1e13236f6757281e
theczipson <krystekp131@interia.pl>
2025-03-16 21:01:27 +0100

    config for jwt

--- server/config.py
+++ server/config.py
@@ -3,9 +3,9 @@ class Config:
     # JUST FOR THE TESTS
     JWT_SECRET_KEY = "0ce63f6a5a7cfc56221c6aa1d0550f720dea54b863f78259a8b39f9214f9fba2"
     JWT_TOKEN_LOCATION = ["cookies"]  # Store JWT in cookies
-    JWT_COOKIE_SECURE = True  # Set to True in production (for HTTPS)
+    JWT_COOKIE_SECURE = False  # Set to True in production (for HTTPS)
     JWT_COOKIE_HTTPONLY = True  # Prevent JavaScript access
     JWT_COOKIE_SAMESITE = "Lax"  # Prevent CSRF issues
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://dummy_user:dummy_secure_password@localhost:5432/petbuddies"
+    SQLALCHEMY_DATABASE_URI = "postgresql://dummy_user:dummy_secure_password@0.0.0.0:5432/petbuddies"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
