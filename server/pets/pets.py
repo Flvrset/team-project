76453e4378aa2eb8a62b604626f4ccb3f2088acc
theczipson <krystekp131@interia.pl>
2025-03-18 19:26:10 +0100

    pet paths

---
+++ server/pets/pets.py
@@ -0,0 +1,76 @@
+from flask import request, jsonify, Blueprint
+from db_models.database_tables import Pet, OwnerShip, User
+from app import db
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity,
+)
+import sqlalchemy
+from sqlalchemy import func
+
+pet = Blueprint("pet", __name__)
+
+
+@pet.route("/add_pet", methods=["POST"])
+@jwt_required()
+def add_pet():
+    pet_name = request.json.get("pet_name", None)
+    type = request.json.get("type", None)
+    race = request.json.get("race", None)
+    size = request.json.get("size", None)
+
+    try:
+        age = int(request.json.get("age", None))
+    except ValueError:
+        return jsonify({"msg": "Podany wiek nie jest liczbą"})
+
+    new_pet = Pet(pet_name=pet_name, type=type, race=race, size=size, age=age)
+
+    try:
+        db.session.add(new_pet)
+
+        ownership = OwnerShip(user_id=get_jwt_identity(), pet_id=new_pet.pet_id)
+        db.session.add(ownership)
+        db.session.commit()
+
+        return jsonify({"msg": "Profil zwierzaka dodany!"}), 201
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        return jsonify({"msg": "Nie można w tej chwili dodać rekordu."}), 406
+
+
+@pet.route("/get_pet_data/<int:pet_id>", methods=["GET"])
+def get_pet_data(pet_id):
+    pet_info = (
+        db.session.query(Pet, func.array_agg(User.login).label("user_login"))
+        .join(OwnerShip, Pet.pet_id == OwnerShip.pet_id)
+        .join(User, OwnerShip.user_id == User.user_id)
+        .filter(Pet.pet_id == pet_id)
+        .group_by(Pet.pet_id)
+        .all()
+    )
+
+    if not pet_info:
+        return jsonify({"msg": "Zwierzak nie został znaleziony!"}), 404
+
+    if len(pet_info) > 1:
+        return (
+            jsonify(
+                {
+                    "msg": "Więcej niż jeden zwierzak o podanym id! Proszę wysłać zgłoszenie do administracji"
+                }
+            ),
+            403,
+        )
+
+    resp_pet, resp_users = pet_info[0]
+
+    return jsonify(
+        {
+            "pet_name": resp_pet.pet_name,
+            "type": resp_pet.type,
+            "race": resp_pet.race,
+            "size": resp_pet.size,
+            "user_logins": resp_users,
+        }
+    )
