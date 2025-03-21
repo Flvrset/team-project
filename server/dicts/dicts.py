4347461ab262b3330c6ef09c47969b62dd1623f0
theczipson <krystekp131@interia.pl>
2025-03-21 22:54:03 +0100

    city endpoint changed

--- server/dicts/dicts.py
+++ server/dicts/dicts.py
@@ -4,10 +4,12 @@ from db_models.database_tables import DPostalCode
 dicts = Blueprint("dicts", __name__)
 
 
-@dicts.route("/city/<string:postal_code>", methods=["GET"])
-def city(postal_code):
+@dicts.route("/city/<string:place>", methods=["GET"])
+def city(place):
     places = DPostalCode.query.filter(
-        DPostalCode.postal_code.like(f"%{postal_code}%")
+        DPostalCode.postal_code.like(f"%{place}%")
+        if place[0].isnumeric() else
+        DPostalCode.place.like(f"%{place}%")
     ).all()
     response = [
         {"place": place.place, "postal_code": place.postal_code} for place in places
